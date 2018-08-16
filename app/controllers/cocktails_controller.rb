7d9c7042919f09314c9fdd5273e1f613cc0d760f
Christian Rognstad <gudim87@hotmail.com>
2018-08-16 19:18:01 +0200

    added figaro gem for idetification problems postgresql

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,38 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :update, :edit, :destroy]
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+puts 'Open & parse Json...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+file = open(url).read
+
+result = JSON.parse(file)
+ingredients = result['drinks']
+puts 'Creating DB...'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+ingredients.each do |ingredient|
+ Ingredient.create(name: "#{ingredient['strIngredient1']}")
+end
+
+cocktails = [
+  {
+    name:         'Mojito',
+
+  },
+  {
+    name:         'Spritz',
+
+  },
+  {
+    name:         'Gin&Tonic',
+
+  },
+  {
+    name:         'Passion',
+
+  },
+  {
+    name:         'Long drinks',
+
+  }
+]
+Cocktail.create!(cocktails)
+
+
+
+puts 'Finished!'
